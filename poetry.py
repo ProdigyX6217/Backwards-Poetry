b49bab594dbeb3bdb9606d78e134a6394431bc29
ProdigyX6217 <53795099+ProdigyX6217@users.noreply.github.com>
2019-10-31 11:56:19 -0700

    Add files via upload

---
+++ poetry.py
@@ -0,0 +1,13 @@
+poem = f"""[If you are a dreamer, come in,
+If you are a dreamer, a wisher, a liar,
+A hope-er, a pray-er, a magic bean buyer…
+If you’re a pretender, come sit by my fire
+For we have some flax-golden tales to spin.
+Come in!
+Come in!]"""
+
+
+def lines_printed_backwards():
+
+
+def lines_printed_random():
